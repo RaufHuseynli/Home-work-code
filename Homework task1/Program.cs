eda559aa3c05e1081d8ad0f86a0df504c0124441
unknown <raufahu@code.edu.azgit config --global user.name RaufHuseynli>
2022-05-20 19:51:07 +0400

    first commit

---
+++ Homework task1/Program.cs
@@ -0,0 +1,102 @@
+﻿#region Task1
+//Console.WriteLine("Birinci regemi daxil ediniz");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Ikinci regem daxil ediniz");
+//int b = int.Parse(Console.ReadLine());
+//int sum = 0;
+//for (int i = a; i < b; i++)
+//{
+//sum=sum+i;
+//}
+//Console.WriteLine("Regemlerin cemi:" +sum);
+
+#endregion
+
+
+#region Task 2
+//Console.WriteLine("Birinci regemi daxil ediniz");
+//float a = float.Parse(Console.ReadLine());
+//Console.WriteLine("Ikinci regem daxil ediniz");
+//float b = float.Parse(Console.ReadLine());
+//float sum = 0;
+//float count = 0;
+//for (float i = a; i < b; i++)
+//{
+//    sum = sum + i;
+//    count++;
+
+//}
+//Console.WriteLine("Regemlerin cemi:" + sum / count);
+
+#endregion
+
+
+
+
+
+#region Task 3
+//bool isContinue = true;
+//do
+//{
+//    Console.WriteLine("Yasinizi daxil edin:");
+//    int age = int.Parse(Console.ReadLine());
+
+
+//    if (age > 18)
+//    {
+//        Console.WriteLine("Ingilis dili balinizi daxil edin:");
+//        int point = int.Parse(Console.ReadLine());
+//        if (point > 50)
+//        {
+//            Console.WriteLine("Kursa qeydiyyata alindiniz.");
+//        }
+//        else
+//        {
+//            Console.WriteLine("Qeydiyyat ucun Ingilis dili baliniz kifayet deyil.");
+//        }
+//    }
+//    else
+//    {
+//        Console.WriteLine("Yasiniz qeydiyyat ucun kifayet deyil. ");
+
+//    }
+
+//    Console.WriteLine("Basqa telebe varmi? :");
+//    string sorgu = Console.ReadLine();
+//    if (sorgu == "he")
+//    {
+//        isContinue = true;
+//    }
+//    else if (sorgu == "yox")
+//    {
+//        Console.WriteLine("Diggetiniz ucun tesekkur edirik");
+//        isContinue = false;
+//    }
+//    else
+//    {
+//        Console.WriteLine("Duzgun ifade daxil edin zehmet olmasa");
+//        isContinue = false;
+//    }
+
+//} while (isContinue);
+
+#endregion
+
+
+
+
+
+#region Task 4
+//Console.WriteLine("Birinci ededi daxil edin:");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Ikinci ededi daxil edin:");
+//int b = int.Parse(Console.ReadLine());
+//int sum = 0;
+//int count = 0;
+//for (int i = a; i < b; i += 2)
+//{
+//    sum = sum + i;
+//}
+//Console.WriteLine($"Tek ededlerin cemi{a} dan ve {b} ye:{ sum}");
+//Console.ReadLine();
+#endregion
